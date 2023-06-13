8e43d91ec5c5a15b767b92bc537a4380a19e8a7a
Ana Carla Quallio Rosa <anacarlarosa@alunos.utfpr.edu.br>
2023-06-13 17:09:29 -0300

    alteracoes

--- teste.rkt
+++ teste.rkt
@@ -16,11 +16,18 @@
 (define (scrape-multiple-websites urls-xpaths output-file)
   (call-with-output-file output-file
     (lambda (output-port)
-      (for-each (lambda (url-xpath)
-                  (let ((url (car url-xpath))
-                        (xpath (cdr url-xpath)))
-                    (scrape-and-append output-port url xpath)))
-                urls-xpaths))))
+      (scrape-and-append-all output-port urls-xpaths))))
+
+(define (scrape-and-append-all output-port urls-xpaths)
+  (cond ((null? urls-xpaths)
+         'done) ; Base case: quando a lista está vazia, terminamos
+        (else
+         (let ((url-xpath (car urls-xpaths))
+               (remaining-urls-xpaths (cdr urls-xpaths)))
+           (let ((url (car url-xpath))
+                 (xpath (cdr url-xpath)))
+             (scrape-and-append output-port url xpath))
+           (scrape-and-append-all output-port remaining-urls-xpaths)))))
 
 (if (file-exists? output-file)
     (printf "O arquivo de saída já existe\n")
