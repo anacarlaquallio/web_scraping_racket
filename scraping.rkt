0e7f295f1dfddf38c495146896565c20809abf45
Ana Carla Quallio Rosa <anacarlarosa@alunos.utfpr.edu.br>
2023-06-13 17:41:03 -0300

    comentarios

--- scraping.rkt
+++ scraping.rkt
@@ -9,6 +9,7 @@
 (require (prefix-in h: html)
          (prefix-in x: xml))
 
+; Obtem parágrafos diante da palavra chave de forma recursiva
 (define (get-paragraphs-as-string lst keyword)
   (cond
     ((null? lst) "")  ; Caso base: lista vazia
@@ -22,22 +23,26 @@
                (get-paragraphs-as-string (cdr lst) keyword)))
          (string-append "      <li>" (car lst) "</li>\n" (get-paragraphs-as-string (cdr lst) keyword))))))
 
+; Extração do título da página
 (define (get-title lst)
   (cond
     ((null? lst) "")  ; Caso base: lista vazia
     (else
      (string-append (car lst) (get-title (cdr lst))))))
 
+; Obtenção SXML da URL
 (define (scrape-website keyword xpath base-url)
   (define my-html
     (html->xexp
      (get-pure-port
       (string->url base-url))))
 
-  (define paragraphs-string (get-paragraphs-as-string ((sxpath xpath) my-html) keyword))
-  (define title-string (get-title ((sxpath "/html/head/title/text()") my-html)))
+; Funções de extração de parágrafos e título, conforme XPath especificado em teste.rkt
+(define paragraphs-string (get-paragraphs-as-string ((sxpath xpath) my-html) keyword))
+(define title-string (get-title ((sxpath "/html/head/title/text()") my-html)))
 
-  (define output-html
+; Geração do arquivo HTML de saída
+(define output-html
     (string-append
      "<html>\n"
      "  <head>\n<title>Resultado do Web Scraping</title>\n</head>\n"
@@ -49,9 +54,9 @@
      "  </body>\n"
      "</html>\n"
     )
-  )
-
+)
   output-html
 )
 
+; Para a importação em outros arquivos
 (provide scrape-website)
--- teste.rkt
+++ teste.rkt
@@ -1,14 +1,18 @@
 #lang racket
 (require "scraping.rkt")
 
+; Palavra-chave
 (define keyword "rose")
 
+; Definição URL e XPath
 (define urls-xpaths
   `(("https://www.bioexplorer.net/plants/flowers/" . "//h3//text()")
     ("https://www.allaboutgardening.com/types-of-flowers/" . "//h2/strong/text()")))
 
+; Definição do arquivo de saída
 (define output-file "resultado.html")
 
+; Processamento par URL e XPath
 (define (scrape-and-append output-port url xpath)
   (let ((output-html (scrape-website keyword xpath url)))
     (display output-html output-port)))
@@ -19,16 +23,16 @@
       (scrape-and-append-all output-port urls-xpaths))))
 
 (define (scrape-and-append-all output-port urls-xpaths)
-  (cond ((null? urls-xpaths)
-         'done) ; Base case: quando a lista está vazia, terminamos
-        (else
-         (let ((url-xpath (car urls-xpaths))
-               (remaining-urls-xpaths (cdr urls-xpaths)))
-           (let ((url (car url-xpath))
-                 (xpath (cdr url-xpath)))
-             (scrape-and-append output-port url xpath))
-           (scrape-and-append-all output-port remaining-urls-xpaths)))))
+  (if (null? urls-xpaths)
+      'done ; Base case: quando a lista está vazia, terminamos
+      (begin
+        (let ((url-xpath (car urls-xpaths)))
+          (let ((url (car url-xpath))
+                (xpath (cdr url-xpath)))
+            (scrape-and-append output-port url xpath)))
+        (scrape-and-append-all output-port (cdr urls-xpaths)))))
 
+; Geração arquivo de saída
 (if (file-exists? output-file)
     (printf "O arquivo de saída já existe\n")
     (begin
